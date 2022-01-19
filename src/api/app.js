9c68ee9ecfc7660a34ac4a1c8faa29300de36234
mariananbp <mariananbp@gmail.com>
2022-01-18 22:19:47 -0300

    Estrutura inicial

--- src/api/app.js
+++ src/api/app.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const users = require('../routes/users');
+const errorMiddleware = require('../middlewares/errorMiddleware');
 
 const app = express();
+
+app.use(express.json());
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
   response.send();
 });
 // Não remover esse end-point, ele é necessário para o avaliador
+
+app.use('/users', users);
+app.use(errorMiddleware);
 
 module.exports = app;
---
+++ src/models/connection.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const mongodb = require('mongodb').MongoClient;
+
+const MONGO_DB_URL = `mongodb://${process.env.HOST || 'mongodb'}:27017/Cookmaster`;
+const DB_NAME = 'Cookmaster';
+
+module.exports = () => mongodb.connect(MONGO_DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then((connection) => connection.db(DB_NAME))
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
